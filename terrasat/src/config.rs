c0ea3c233e0a6e0ed4695ad1db23e3335ddb987c
Brendan Kapp <brendankapp@protonmail.com>
2023-03-14 08:45:07 -0400

    Update config to parse input more fully

--- terrasat/src/config.rs
+++ terrasat/src/config.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Formatter, self, Display};
 use std::hash::Hash;
 use std::path::PathBuf;
-use std::{fs};
+use std::{fs, vec};
 use std::io::Error as IoError;
 use serde::{ Serialize, Deserialize };
 use toml;
@@ -37,7 +37,13 @@ struct Satellite {
     os: String,
     debug_mode: bool,
     password: String,
-    connection_limit: i32,
+    connection_limit: usize,
+}
+
+impl Satellite {
+	pub fn empty() -> Self {
+		Satellite { os: String::from("Unknown"), debug_mode: false, password: String::from("Unknown"), connection_limit: 0 }
+	}
 }
 
 
@@ -78,7 +84,10 @@ impl Config {
 		{
 			satellites: match config_toml.satellites {
 				Some(satellites) => {
-					let mut vec = Vec::with_capacity(satellites.len());
+					let mut vec: Vec<Satellite> = Vec::with_capacity(satellites.len());
+					for _ in 0..satellites.len() {
+						vec.push(Satellite::empty());
+					}
 					for (name, sat) in satellites {
 						let index = name[3..].parse::<usize>();
 						if index.is_err() {
@@ -86,12 +95,14 @@ impl Config {
 							+ ". They need to named sat0...satn without skipping any #."
 							)))
 						}
-						if index.unwrap() > vec.len() {
-							return Err(ConfigParseError(String::from("Satellite index out of bounds for ".to_owned() + &name
+						let index = index.unwrap();
+						if index > vec.len() {
+							return Err(ConfigParseError(String::from("Satellite index out of bounds for ".to_owned()
+							+ &name + " at index " + &index.to_string()
 							+ ". They need to named sat0...satn without skipping any #."
 							)))
 						}
-						vec.push(sat);
+						vec[index] = sat;
 					}
 					vec
 				}
