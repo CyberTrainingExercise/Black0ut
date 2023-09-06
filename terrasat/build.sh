1a12103817119c4e47e26759adf3dcbefc6db690
Brendan Kapp <brendankapp@protonmail.com>
2023-09-06 12:26:31 -0500

    Add working build system

---
+++ terrasat/build.sh
@@ -0,0 +1,8 @@
+# builds both the client and the server executables
+cargo build --release
+cp target/release/terrasat builds/
+cp target/release/server builds/
+chmod +x builds/terrasat
+chmod +x builds/server
+cp cli-view/src/config.toml builds/cli-config.toml
+cp server/src/config.toml builds/server-config.toml
